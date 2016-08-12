435f92764f447bc3130bc8cc8fbe25a06e8a7140
Ayana Powell <Ayana@Ayanas-Macbook.local>
2016-08-12 10:48:27 -0700

    added more jquery to other divs

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,39 @@ $(document).ready(function(){
 var webDev = $('.web-dev'),
 		webWork = $('.web-work'),
 		mobileDev = $('.mobile-dev'),
-		desktopDev = $('.desktop-dev');
+		softwareDev = $('.software-dev'),
+    choices = document.getElementById('choices');
+	 
+	function initialSelection() {
+		choices.addEventListener('click', function(evt) {
+			var target = evt.target;
+			if (target.id === 'web') {
+		    webDev.addClass("show");
+		  } else if (target.id === 'mobile') {
+		    mobileDev.addClass("show");
+		  } else if (target.id === 'software'){
+		    softwareDev.addClass("show");
+		  }
+		    }, false);
+		};
+		initialSelection();	
+
+		function webDevSelection() {
+			choices.addEventListener('click', function(evt) {
+				var target = evt.target;
+				if (target.id === 'looks') {
+					alert("You should take CSS/Design!");
+				} else if (target.id === 'works') {
+					webWork.addClass('show');
+					$('html, body').animate({
+						scrollTop: webWork.offset().top 
+					}, 1000)
+				} 
+			}, false);
+		};
+		webDevSelection();
 
 		
-	 
-	function findSelection() {
-		document.getElementById('choices').addEventListener('click', function(evt) {
-    		var target = evt.target;
-			if (target.id === 'web') {
-		        webWork.addClass("show");
-		        $('html, body').animate({
-					scrollTop: $('#about_fade').offset().top
-				}, 1000)
-		    } else if (target.id === 'mobile') {
-		    	mobileDev.addClass("show");
-		    } else {
-		        desktopDev.addClass("show");
-		    }
-		}, false);
-		};
-		findSelection();	
-
 });
 
 
